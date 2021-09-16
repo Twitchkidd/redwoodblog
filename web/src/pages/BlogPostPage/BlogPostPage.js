7f45693bc78cbc178c98ded7607da3c9b69e7389
Gareth Field <field.gareth@gmail.com>
2021-09-16 08:50:09 -0400

    create route param type

--- web/src/pages/BlogPostPage/BlogPostPage.js
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -1,7 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import BlogPostCell from 'src/components/BlogPostCell'
 
-const BlogPostPage = () => {
+const BlogPostPage = ({ id }) => {
   return (
     <>
       <MetaTags
@@ -10,15 +10,7 @@ const BlogPostPage = () => {
         /* you should un-comment description and add a unique description, 155 characters or less
       You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
       />
-
-      <h1>BlogPostPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/BlogPostPage/BlogPostPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>blogPost</code>, link to me with `
-        <Link to={routes.blogPost()}>BlogPost</Link>`
-      </p>
+      <BlogPostCell id={id} />
     </>
   )
 }
