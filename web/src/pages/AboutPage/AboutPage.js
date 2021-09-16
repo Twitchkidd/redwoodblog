5b91b50c8cc8083ebdbc08285dc6a4521a83b8dc
Gareth Field <field.gareth@gmail.com>
2021-09-16 04:42:25 -0400

    first layout!

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -10,20 +10,8 @@ const AboutPage = () => {
         /* you should un-comment description and add a unique description, 155 characters or less
       You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
       />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>This site was created to learn Redwood! Hooray!</p>
-        <Link to={routes.home()}>Return home</Link>
-      </main>
+      <p>This site was created to learn Redwood! Hooray!</p>
+      <Link to={routes.home()}>Return home</Link>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -10,17 +10,7 @@ const HomePage = () => {
         /* you should un-comment description and add a unique description, 155 characters or less
       You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
       />
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-        <main>Home</main>
-      </header>
+      Home
     </>
   )
 }
