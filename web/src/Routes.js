5079e27be440e0102ff6f0683ba786528ba5e9e0
Gareth Field <field.gareth@gmail.com>
2021-09-18 08:35:04 -0400

    what are you doin there?

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -15,13 +15,13 @@ const Routes = () => {
   return (
     <Router>
       <Set wrap={PostsLayout}>
-        <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
         <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
         <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Set wrap={BlogLayout}>
+        <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
         <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
